459662df891ba6e5da8c746652181a880dcdbbee
육기통 <yukkitong@yuggitong-ui-MacBook-Pro.local>
2017-11-30 02:12:28 +0900

    add untracked files App.js and index.js

--- index.js
+++
@@ -1,8 +0,0 @@
-import { AppRegistry } from 'react-native';
-<<<<<<< HEAD
-import App from './app/AvatarWrapper';
-=======
-import AppIntro from './app/AppIntro.js';
->>>>>>> 361851c2886874bdf6a262b1c4f63f84d8d7ba8d
-
-AppRegistry.registerComponent('Example01', () => App);
