70585666a37f2316f1e60a1d77e5a7cab0242a40
육기통 <yukkitong@yuggitong-ui-MacBook-Pro.local>
2017-12-04 19:40:13 +0900

    new FavoriteGender.js

---
+++ app/FavoriteGender.js
@@ -0,0 +1,26 @@
+import React from 'react';
+import {
+  StyleSheet,
+  Text,
+  View,
+} from 'react-native';
+import PropTypes from 'prop-types';
+
+export default class FavoriteGender extends React.Component {
+
+  constructor(props) {
+    super(props);
+  }
+
+  render() {
+    return (
+      <View style={styles.container}>
+
+      </View>
+    );
+  }
+}
+
+const styles = StyleSheet.create({
+
+});
