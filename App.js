89f67e63a8e0024edc916bea8fbe6bbdff4196b8
육기통 <yukkitong@yuggitong-ui-MacBook-Pro.local>
2017-12-10 21:15:30 +0900

    update SiteMap.js and add Modals.js

--- App.js
+++ App.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import { Router, Scene, Modal, Stack } from 'react-native-router-flux';
+import { Router, Scene } from 'react-native-router-flux';
 import SiteMap from './app/scene/SiteMap';
 import Login from './app/scene/Login';
 import Register from './app/scene/Register';
@@ -9,19 +9,6 @@ import Users from './app/scene/Users';
 import UserDetail from './app/scene/UserDetail';
 import Moment from './app/scene/Moment';
 import MomentDetail from './app/scene/MomentDetail';
-import TextChat from './app/scene/TextChat';
-import Notice from './app/scene/Notice';
-import FreeCharge from './app/scene/FreeCharge';
-import Store from './app/scene/Store';
-import NotificationSetting from './app/scene/NotificationSetting';
-import FavoriteGender from './app/scene/FavoriteGender';
-import FavoriteCountry from './app/scene/FavoriteCountry';
-import Followers from './app/scene/Followers';
-import Inquiry from './app/scene/Inquiry';
-import PayHistory from './app/scene/PayHistory';
-import Contract from './app/scene/Contract';
-import About from './app/scene/About';
-import RequestCash from './app/scene/RequestCash';
 
 export default () => {
   return (
@@ -36,19 +23,6 @@ export default () => {
         <Scene key="userDetail" title="User Detail" component={UserDetail} />
         <Scene key="moment" title="Moment" component={Moment} />
         <Scene key="momentDetail" title="Moment Detail" component={MomentDetail} />
-        <Scene key="textChat" title="Text Chat" component={TextChat} />
-        <Scene key="notice" title="Notice" component={Notice} />
-        <Scene key="freeCharge" title="Free Charge" component={FreeCharge} />
-        <Scene key="store" title="Store" component={Store} />
-        <Scene key="notificationSetting" title="Notification Setting" component={NotificationSetting} />
-        <Scene key="favoriteGender" title="Favorite Gender" component={FavoriteGender} />
-        <Scene key="favoriteCountry" title="Favorite Country" component={FavoriteCountry} />
-        <Scene key="followers" title="Followers" component={Followers} />
-        <Scene key="inquiry" title="Inquiry" component={Inquiry} />
-        <Scene key="payHistory" title="Payment History" component={PayHistory} />
-        <Scene key="contract" title="Contract" component={Contract} />
-        <Scene key="about" title="About" component={About} />
-        <Scene key="requestCash" title="Request Cash" component={RequestCash} />
       </Scene>
     </Router>
   );
--- app/scene/SiteMap.js
+++ app/scene/SiteMap.js
@@ -1,81 +1,134 @@
 import React from 'react';
+
 import { 
   StyleSheet, 
   ScrollView, 
   TouchableOpacity, 
+  View,
   Text 
 } from 'react-native';
+
 import { Actions } from 'react-native-router-flux';
 
+import { 
+  About as AboutModal,
+  Contract as ContractModal,
+  PayHistory as PayHistoryModal,
+  Inquiry as InquiryModal,
+  FavGender as FavGenderModal,
+  FavCountry as FavCountryModal,
+  NotiSetting as NotiSettingModal,
+  Followers as FollowersModal,
+  UserInfo as UserInfoModal,
+  Store as StoreModal,
+  FreeCharging as FreeChargingModal,
+  TextChatting as TextChattingModal,
+  Notice as NoticeModal,
+  RequestCash as RequestCashModal,
+} from './Modals';
+
 export default class SiteMap extends React.Component {
+  
+  modal = {};
 
   render() {
     return (
-      <ScrollView style={{ flex: 1 }}>
-        <TouchableOpacity onPress={() => Actions.login() }>
-          <Text style={styles.button}>로그인</Text>
-        </TouchableOpacity>
-        <TouchableOpacity onPress={() => Actions.register() }>
-          <Text style={styles.button}>회원가입</Text>
-        </TouchableOpacity>
-        <TouchableOpacity onPress={() => Actions.overview() }>
-          <Text style={styles.button}>오버뷰</Text>
-        </TouchableOpacity>
-        <TouchableOpacity onPress={() => Actions.chatHome() }>
-          <Text style={styles.button}>채팅홈</Text>
-        </TouchableOpacity>
-        <TouchableOpacity onPress={() => Actions.users() }>
-          <Text style={styles.button}>사용자-리스트</Text>
-        </TouchableOpacity>
-        <TouchableOpacity onPress={() => Actions.userDetail() }>
-          <Text style={styles.button}>사용자-상세</Text>
-        </TouchableOpacity>
-        <TouchableOpacity onPress={() => Actions.moment() }>
-          <Text style={styles.button}>모멘트-리스트</Text>
-        </TouchableOpacity>
-        <TouchableOpacity onPress={() => Actions.momentDetail() }>
-          <Text style={styles.button}>모멘트-상세</Text>
-        </TouchableOpacity>
-        <TouchableOpacity onPress={() => Actions.textChat() }>
-          <Text style={styles.button}>텍스트 챗팅</Text>
-        </TouchableOpacity>
-        <TouchableOpacity onPress={() => Actions.notice() }>
-          <Text style={styles.button}>공지사항</Text>
-        </TouchableOpacity>
-        <TouchableOpacity onPress={() => Actions.freeCharge() }>
-          <Text style={styles.button}>무료충전</Text>
-        </TouchableOpacity>
-        <TouchableOpacity onPress={() => Actions.store() }>
-          <Text style={styles.button}>상점</Text>
-        </TouchableOpacity>
-        <TouchableOpacity onPress={() => Actions.notificationSetting() }>
-          <Text style={styles.button}>알림설정</Text>
-        </TouchableOpacity>
-        <TouchableOpacity onPress={() => Actions.favoriteGender() }>
-          <Text style={styles.button}>선호성별</Text>
-        </TouchableOpacity>
-        <TouchableOpacity onPress={() => Actions.favoriteCountry() }>
-          <Text style={styles.button}>선호지역</Text>
-        </TouchableOpacity>
-        <TouchableOpacity onPress={() => Actions.followers() }>
-          <Text style={styles.button}>팔로워</Text>
-        </TouchableOpacity>
-        <TouchableOpacity onPress={() => Actions.inquiry() }>
-          <Text style={styles.button}>문의하기</Text>
-        </TouchableOpacity>
-        <TouchableOpacity onPress={() => Actions.payHistory() }>
-          <Text style={styles.button}>이용내역</Text>
-        </TouchableOpacity>
-        <TouchableOpacity onPress={() => Actions.contract() }>
-          <Text style={styles.button}>이용약관</Text>
-        </TouchableOpacity>
-        <TouchableOpacity onPress={() => Actions.about() }>
-          <Text style={styles.button}>저작권명시</Text>
-        </TouchableOpacity>
-        <TouchableOpacity onPress={() => Actions.requestCash() }>
-          <Text style={styles.button}>캐시전환</Text>
-        </TouchableOpacity>
-      </ScrollView>
+      <View style={{ flex: 1 }}>
+        <ScrollView style={{ flex: 1 }}>
+          <TouchableOpacity onPress={() => Actions.login() }>
+            <Text style={styles.button}>로그인</Text>
+          </TouchableOpacity>
+          <TouchableOpacity onPress={() => Actions.register() }>
+            <Text style={styles.button}>회원가입</Text>
+          </TouchableOpacity>
+          <TouchableOpacity onPress={() => Actions.overview() }>
+            <Text style={styles.button}>오버뷰</Text>
+          </TouchableOpacity>
+          <TouchableOpacity onPress={() => Actions.chatHome() }>
+            <Text style={styles.button}>채팅홈</Text>
+          </TouchableOpacity>
+          <TouchableOpacity onPress={() => Actions.users() }>
+            <Text style={styles.button}>사용자-리스트</Text>
+          </TouchableOpacity>
+          <TouchableOpacity onPress={() => Actions.userDetail() }>
+            <Text style={styles.button}>사용자-상세</Text>
+          </TouchableOpacity>
+          <TouchableOpacity onPress={() => Actions.moment() }>
+            <Text style={styles.button}>모멘트-리스트</Text>
+          </TouchableOpacity>
+          <TouchableOpacity onPress={() => Actions.momentDetail() }>
+            <Text style={styles.button}>모멘트-상세</Text>
+          </TouchableOpacity>
+          <TouchableOpacity
+            onPress={() => this.modal['text_chatting'].open() }>
+            <Text style={styles.button}>텍스트 챗팅</Text>
+          </TouchableOpacity>
+          <TouchableOpacity
+            onPress={() => this.modal['notice'].open() }>
+            <Text style={styles.button}>공지사항</Text>
+          </TouchableOpacity>
+          <TouchableOpacity 
+            onPress={() => this.modal['free_charging'].open() }>
+            <Text style={styles.button}>무료충전</Text>
+          </TouchableOpacity>
+          <TouchableOpacity   
+            onPress={() => this.modal['store'].open() }>
+            <Text style={styles.button}>상점</Text>
+          </TouchableOpacity>
+          <TouchableOpacity
+            onPress={() => this.modal['noti_setting'].open() }>
+            <Text style={styles.button}>알림설정</Text>
+          </TouchableOpacity>
+          <TouchableOpacity
+            onPress={() => this.modal['fav_gender'].open() }>
+            <Text style={styles.button}>선호성별</Text>
+          </TouchableOpacity>
+          <TouchableOpacity
+            onPress={() => this.modal['fav_country'].open() }>
+            <Text style={styles.button}>선호지역</Text>
+          </TouchableOpacity>
+          <TouchableOpacity 
+            onPress={() => this.modal['followers'].open() }>
+            <Text style={styles.button}>팔로워</Text>
+          </TouchableOpacity>
+          <TouchableOpacity 
+            onPress={() => this.modal['inquiry'].open() }>
+            <Text style={styles.button}>문의하기</Text>
+          </TouchableOpacity>
+          <TouchableOpacity
+            onPress={() => this.modal['pay_history'].open() }>
+            <Text style={styles.button}>사용내역</Text>
+          </TouchableOpacity>
+          <TouchableOpacity
+            onPress={() => this.modal['contract'].open() }>
+            <Text style={styles.button}>이용약관</Text>
+          </TouchableOpacity>
+          <TouchableOpacity
+            onPress={() => this.modal['about'].open() }>
+            <Text style={styles.button}>저작권명시</Text>
+          </TouchableOpacity>
+          <TouchableOpacity 
+            onPress={() => this.modal['request_cash'].open() }>
+            <Text style={styles.button}>캐시전환</Text>
+          </TouchableOpacity>
+        </ScrollView>
+
+        <AboutModal ref={(comp) => this.modal['about'] = comp } />
+        <ContractModal ref={(comp) => this.modal['contract'] = comp } />
+        <PayHistoryModal ref={(comp) => this.modal['pay_history'] = comp } />
+        <InquiryModal ref={(comp) => this.modal['inquiry'] = comp } />
+        <FavGenderModal ref={(comp) => this.modal['fav_gender'] = comp } />
+        <FavCountryModal ref={(comp) => this.modal['fav_country'] = comp } />
+        <NotiSettingModal ref={(comp) => this.modal['noti_setting'] = comp } />
+        <FollowersModal ref={(comp) => this.modal['followers'] = comp } />
+        <UserInfoModal ref={(comp) => this.modal['user_info'] = comp } />
+        <StoreModal ref={(comp) => this.modal['store'] = comp } />
+        <FreeChargingModal ref={(comp) => this.modal['free_charging'] = comp } />
+        <NoticeModal ref={(comp) => this.modal['notice'] = comp } />
+        <TextChattingModal ref={(comp) => this.modal['text_chatting'] = comp } />
+        <RequestCashModal ref={(comp) => this.modal['request_cash'] = comp } />
+
+      </View>
     );
   }
 
