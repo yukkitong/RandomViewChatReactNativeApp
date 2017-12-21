b8622861909410bf56da6b6418ee104b73bcdb52
육기통 <yukkitong@yuggitong-ui-MacBook-Pro.local>
2017-12-21 14:22:04 +0900

    add lib

--- android/app/src/main/java/com/example01/MainApplication.java
+++ android/app/src/main/java/com/example01/MainApplication.java
@@ -3,6 +3,7 @@ package com.example01;
 import android.app.Application;
 
 import com.facebook.react.ReactApplication;
+import me.fraserxu.rncouchbaselite.ReactCBLiteManager;
 import com.facebook.react.shell.MainReactPackage;
 import com.facebook.react.ReactNativeHost;
 import com.facebook.react.ReactPackage;
@@ -28,6 +29,7 @@ public class MainApplication extends Application implements ReactApplication {
     protected List<ReactPackage> getPackages() {
       return Arrays.<ReactPackage>asList(
           new MainReactPackage(),
+            new ReactCBLiteManager(),
           //new FIRMessagingPackage(),  // NOTE FCM
           //new RNUUIDGeneratorPackage(),
           new VectorIconsPackage(),
